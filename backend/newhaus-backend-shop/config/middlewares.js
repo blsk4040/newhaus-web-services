ff671d9c0c286b93f5a8c89a28f4d3b26258aec8
testclouds208 <testclouds208@outlook.com>
2026-05-11 00:07:16 +0200

    Allow production frontend in Strapi CORS

--- backend/newhaus-backend-shop/config/middlewares.js
+++ backend/newhaus-backend-shop/config/middlewares.js
@@ -5,7 +5,12 @@ module.exports = ({ env }) => [
   {
     name: 'strapi::cors',
     config: {
-      origin: env.array('STRAPI_CORS_ORIGINS', ['http://127.0.0.1:5501', 'http://localhost:5501']),
+      origin: env.array('STRAPI_CORS_ORIGINS', [
+        'http://127.0.0.1:5501',
+        'http://localhost:5501',
+        'https://nhx.co.za',
+        'https://www.nhx.co.za',
+      ]),
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     },
